9a27142d0fd8922efcbd2115c983e55387f8449c
araki <araki@qoncept.co.jp>
2020-01-17 14:25:58 +0900

    SNConv

--- Sources/SinGAN/CustomLayer.swift
+++ Sources/SinGAN/CustomLayer.swift
@@ -52,29 +52,11 @@ public struct SNConv2D<Scalar: TensorFlowFloatingPoint>: Layer {
     
     @differentiable
     public func callAsFunction(_ input: Tensor<Scalar>) -> Tensor<Scalar> {
-        // FIXME: tensorflow-RELEASE-0.6 has bug in AD (already fixed in nightly).
-        // `f = wBar()` leads 0 gradients.
-        let f: Tensor<Scalar>
-        if enabled {
-            f = conv.filter
-        } else {
-            let outputDim = conv.filter.shape[3]
-            let mat = conv.filter.reshaped(to: [-1, outputDim])
-
-            var u = Tensor<Scalar>(0)
-            for _ in 0..<numPowerIterations {
-                u = l2normalize(matmul(v.value, mat.transposed())) // [1, rows]
-                v.value = l2normalize(matmul(u, mat)) // [1, cols]
-            }
-
-            let sigma = matmul(matmul(u, mat), v.value.transposed()) // [1, 1]
-
-            // Should detach sigma?
-            f = conv.filter / sigma
-        }
+        // WARNING: tensorflow-RELEASE-0.6 has bug in AD (already fixed in nightly).
+        // Gradients for conv.weight will be 0.
         return conv.activation(conv2D(
             input,
-            filter: f,
+            filter: wBar(),
             strides: (1, conv.strides.0, conv.strides.1, 1),
             padding: conv.padding,
             dilations: (1, conv.dilations.0, conv.dilations.1, 1)) + conv.bias)
